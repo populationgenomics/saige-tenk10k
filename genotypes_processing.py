d9667d56e98aa11bcd446b87eea1a8c83f47c125
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2023-08-23 17:54:40 +1000

    comment sharing my frustration

--- genotypes_processing.py
+++ genotypes_processing.py
@@ -271,7 +271,7 @@ def filter_variants(
     # subset variants for variance ratio estimation
     # minor allele count (MAC) > 20
     vre_mt = mt.filter_rows(mt.variant_qc.AC[0] > vre_mac_threshold)
-    print(vre_mac_threshold)
+    # when testing this in a notebook I get 261416 here I keep getting 0
     logging.info(f'Number of variants post AC filter: {vre_mt.count()[0]}')
     if vre_mt.count()[0] == 0:
         return
