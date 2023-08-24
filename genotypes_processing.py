7a3aa8a609f4c15e1cc0632d152e897d78e24ef3
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2023-08-24 13:48:36 +1000

    try set comprehension

--- genotypes_processing.py
+++ genotypes_processing.py
@@ -138,7 +138,7 @@ def get_duplicated_samples(mt: hl.MatrixTable) -> set:
     # Get the SG ID associated with each of the most recent samples
     keep = [sample_sg_map[sample_id] for sample_id in latest_samples]
     keep = [list(sg.values())[0] for sg in keep]
-    keep = set([sublist for list in keep for sublist in list])
+    keep = {sublist for list in keep for sublist in list}
 
     matrix_samples = set(mt.s.collect())
     # dup_samples = matrix_samples[matrix_samples not in keep]
