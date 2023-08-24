5df4092163754216e6460986b00ebd8c55288d6d
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2023-08-24 13:45:14 +1000

    make lists sets

--- genotypes_processing.py
+++ genotypes_processing.py
@@ -138,9 +138,9 @@ def get_duplicated_samples(mt: hl.MatrixTable) -> set:
     # Get the SG ID associated with each of the most recent samples
     keep = [sample_sg_map[sample_id] for sample_id in latest_samples]
     keep = [list(sg.values())[0] for sg in keep]
-    keep = [sublist for list in keep for sublist in list]
+    keep = set([sublist for list in keep for sublist in list])
 
-    matrix_samples = mt.s.collect()
+    matrix_samples = set(mt.s.collect())
     # dup_samples = matrix_samples[matrix_samples not in keep]
     dup_samples = matrix_samples.difference(keep)
     logging.info(f'Number of duplicated samples: {len(set(dup_samples))}')
