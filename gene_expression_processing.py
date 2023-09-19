7eb64142f80c87da061bfebfe12a034fd895a61b
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2023-09-19 11:38:10 +1000

    update remove lowly expressed

--- gene_expression_processing.py
+++ gene_expression_processing.py
@@ -45,33 +45,48 @@ logging.basicConfig(
 
 # adapted from https://github.com/populationgenomics/tob-wgs/blob/get-variants/
 # scripts/eqtl_hail_batch/generate_eqtl_spearman.py#L34-L60
-def filter_lowly_expressed_genes(expression_df, min_pct=5):
+# def filter_lowly_expressed_genes(expression_df, min_pct=5):
+#     """Remove genes with low expression across cells
+
+#     Input:
+#     expression_df: a data frame with cells as rows and genes as columns,
+#     containing normalised expression values (i.e., the number of molecules
+#     for each gene detected in each cell, normalised by sequencing depth).
+
+#     Returns:
+#     A filtered version of the input data frame, after removing columns (genes)
+#     expressed in less than {min_pct}% of the rows (cells).
+#     """
+
+#     # Remove genes with 0 expression in all cells
+#     expression_df = expression_df.loc[:, (expression_df != 0).any(axis=0)]
+#     genes_not_equal_zero = expression_df.iloc[:, 1:].values != 0
+#     n_expr_over_zero = pd.DataFrame(genes_not_equal_zero.sum(axis=0))
+#     percent_expr_over_zero = (n_expr_over_zero / len(expression_df.index)) * 100
+#     percent_expr_over_zero.index = expression_df.columns[1:]
+
+#     # Filter genes with less than {min_pct} percent cells with non-zero expression
+#     atleastminpercent = percent_expr_over_zero[(percent_expr_over_zero > min_pct)[0]]
+#     sample_ids = expression_df['sampleid']
+#     expression_df = expression_df[atleastminpercent.index]
+#     expression_df.insert(loc=0, column='sampleid', value=sample_ids)
+
+#     return expression_df
+
+
+def filter_lowly_expressed_genes(expression_adata, min_pct=5):
     """Remove genes with low expression across cells
 
-    Input:
-    expression_df: a data frame with cells as rows and genes as columns,
-    containing normalised expression values (i.e., the number of molecules
-    for each gene detected in each cell, normalised by sequencing depth).
+    Input: adata with all genes
 
-    Returns:
-    A filtered version of the input data frame, after removing columns (genes)
-    expressed in less than {min_pct}% of the rows (cells).
+    Output: adata filtered
     """
+    import math
 
-    # Remove genes with 0 expression in all cells
-    expression_df = expression_df.loc[:, (expression_df != 0).any(axis=0)]
-    genes_not_equal_zero = expression_df.iloc[:, 1:].values != 0
-    n_expr_over_zero = pd.DataFrame(genes_not_equal_zero.sum(axis=0))
-    percent_expr_over_zero = (n_expr_over_zero / len(expression_df.index)) * 100
-    percent_expr_over_zero.index = expression_df.columns[1:]
-
-    # Filter genes with less than {min_pct} percent cells with non-zero expression
-    atleastminpercent = percent_expr_over_zero[(percent_expr_over_zero > min_pct)[0]]
-    sample_ids = expression_df['sampleid']
-    expression_df = expression_df[atleastminpercent.index]
-    expression_df.insert(loc=0, column='sampleid', value=sample_ids)
-
-    return expression_df
+    n_all_cells = len(expression_adata.obs.index)
+    min_cells = math.ceil((n_all_cells * min_pct) / 100)
+    expression_adata = sc.pp.filter_genes(expression_adata, min_cells=min_cells)
+    return expression_adata
 
 
 def get_chrom_celltype_expression(
@@ -182,7 +197,8 @@ def get_gene_cis_file(gene_info_df, gene: str, window_size: int):
         hl.get_reference('GRCh38').lengths[chrom],
     )
     data = {'chromosome': chrom, 'start': left_boundary, 'end': right_boundary}
-    gene_cis_df = pd.DataFrame(data, index=gene)
+    # check if I need an index at all
+    gene_cis_df = pd.DataFrame(data, index=[gene])
     return gene_cis_df
 
 
@@ -222,20 +238,29 @@ def expression_pipeline(
         )
         for chromosome in chromosome_list:
             # get expression (cell type + chromosome)
-            expr_df = get_chrom_celltype_expression(
+            expr_adata = get_chrom_celltype_expression(
                 gene_info_df=gene_info_df,
-                expression_files_prefix=expression_files_prefix,
+                # expression_files_prefix=expression_files_prefix,
                 chromosome=chromosome,
-                cell_type=celltype,
+                # cell_type=celltype,
             )
-            # remove lowly expressed genes
-            expr_df = filter_lowly_expressed_genes(
-                expression_df=expr_df, min_pct=min_pct_expr
-            )
+            # # remove lowly expressed genes
+            # expr_df = filter_lowly_expressed_genes(
+            #     expression_df=expr_df, min_pct=min_pct_expr
+            # )
             # combine files
-            pheno_cov_df = build_pheno_cov_filename(
-                cov_df=cov_df, expression_df=expr_df, smf_df=smf_df
-            )
+            # pheno_cov_df = build_pheno_cov_filename(
+            #     cov_df=cov_df, expression_df=expr_df, smf_df=smf_df
+            # )
+            # extract genes
+            genes = expr_adata.raw.var.index
+            for gene in genes:
+                pheno_cov_df = build_pheno_cov_filename(
+                    gene_name=gene,
+                    cov_df=cov_df,
+                    expression_adata=expr_adata,
+                    smf_df=smf_df,
+                )
 
             # write to output
             pheno_cov_filename = to_path(
