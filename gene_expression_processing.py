af7b51eaa016e093c83a395f9134ad77b0a2f2f5
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2023-09-19 11:51:08 +1000

    black and remove more commented out lines'

--- gene_expression_processing.py
+++ gene_expression_processing.py
@@ -44,37 +44,6 @@ logging.basicConfig(
 )
 
 
-# adapted from https://github.com/populationgenomics/tob-wgs/blob/get-variants/
-# scripts/eqtl_hail_batch/generate_eqtl_spearman.py#L34-L60
-# def filter_lowly_expressed_genes(expression_df, min_pct=5):
-#     """Remove genes with low expression across cells
-
-#     Input:
-#     expression_df: a data frame with cells as rows and genes as columns,
-#     containing normalised expression values (i.e., the number of molecules
-#     for each gene detected in each cell, normalised by sequencing depth).
-
-#     Returns:
-#     A filtered version of the input data frame, after removing columns (genes)
-#     expressed in less than {min_pct}% of the rows (cells).
-#     """
-
-#     # Remove genes with 0 expression in all cells
-#     expression_df = expression_df.loc[:, (expression_df != 0).any(axis=0)]
-#     genes_not_equal_zero = expression_df.iloc[:, 1:].values != 0
-#     n_expr_over_zero = pd.DataFrame(genes_not_equal_zero.sum(axis=0))
-#     percent_expr_over_zero = (n_expr_over_zero / len(expression_df.index)) * 100
-#     percent_expr_over_zero.index = expression_df.columns[1:]
-
-#     # Filter genes with less than {min_pct} percent cells with non-zero expression
-#     atleastminpercent = percent_expr_over_zero[(percent_expr_over_zero > min_pct)[0]]
-#     sample_ids = expression_df['sampleid']
-#     expression_df = expression_df[atleastminpercent.index]
-#     expression_df.insert(loc=0, column='sampleid', value=sample_ids)
-
-#     return expression_df
-
-
 def filter_lowly_expressed_genes(expression_adata, min_pct=5):
     """Remove genes with low expression across cells
 
@@ -123,12 +92,10 @@ def get_chrom_celltype_expression(
 
     # extract all genes
     all_genes = expression_adata.raw.var.index
-    # all_genes = expression_df.columns.values
     # select only genes on relevant chromosome
     genes_chrom = gene_info_df[gene_info_df['chr'] == chromosome].index.values
     common_genes = set(all_genes).intersection(set(genes_chrom))
     # return expression for the correct chromosomes only
-    # return expression_df[:, common_genes]
     return expression_adata[:, common_genes]  # check syntax
 
 
