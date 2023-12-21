b0d9cddc1dfdcf5416b041a38d3e550225f979d1
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2023-12-21 14:29:16 +1100

    point to correct file

--- extract_bioheart_genos_demux.py
+++ extract_bioheart_genos_demux.py
@@ -24,7 +24,9 @@ from cpg_utils.hail_batch import (
 import hail as hl
 from hail.methods import export_vcf
 
-DEFAULT_JOINT_CALL_MT = dataset_path('mt/v7.mt')
+DEFAULT_JOINT_CALL_MT = dataset_path(
+    'vds/0c401a0c6aee07549db7d85f88698fbaae4183_990.vds'
+)
 HAIL_IMAGE = get_config()['images']['scanpy']
 
 # read hail matrix table object (WGS data)
@@ -38,11 +40,11 @@ logging.info(f'Number of total loci: {mt.count()[0]}')
 logging.info(f'Number of total samples: {mt.count()[1]}')
 
 mt = mt.filter_rows(
-        (hl.len(hl.or_else(mt.filters, hl.empty_set(hl.tstr))) == 0)  # QC
-        & (hl.len(mt.alleles) == 2)  # remove hom-ref
-        & (mt.n_unsplit_alleles == 2)  # biallelic (exclude multiallelic)
-        & (hl.is_snp(mt.alleles[0], mt.alleles[1]))  # SNPs (exclude indels)
-    )
+    (hl.len(hl.or_else(mt.filters, hl.empty_set(hl.tstr))) == 0)  # QC
+    & (hl.len(mt.alleles) == 2)  # remove hom-ref
+    & (mt.n_unsplit_alleles == 2)  # biallelic (exclude multiallelic)
+    & (hl.is_snp(mt.alleles[0], mt.alleles[1]))  # SNPs (exclude indels)
+)
 
 mt = hl.variant_qc(mt)
 
