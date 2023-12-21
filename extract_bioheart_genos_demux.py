2b0a8d9389c1e6db285ef73997c9f80c8004613c
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2023-12-21 14:41:04 +1100

    mt to vds

--- extract_bioheart_genos_demux.py
+++ extract_bioheart_genos_demux.py
@@ -24,7 +24,7 @@ from cpg_utils.hail_batch import (
 import hail as hl
 from hail.methods import export_vcf
 
-DEFAULT_JOINT_CALL_MT = dataset_path(
+BIOHEART_JOINT_CALL_VDS = dataset_path(
     'vds/0c401a0c6aee07549db7d85f88698fbaae4183_990.vds'
 )
 HAIL_IMAGE = get_config()['images']['scanpy']
@@ -32,24 +32,24 @@ HAIL_IMAGE = get_config()['images']['scanpy']
 # read hail matrix table object (WGS data)
 init_batch()
 
-mt_path = DEFAULT_JOINT_CALL_MT
+vds_path = BIOHEART_JOINT_CALL_VDS
 cv_demux_vcf_path = output_path('demux_vcf_common_variants')
 
-mt = hl.read_matrix_table(mt_path)
-logging.info(f'Number of total loci: {mt.count()[0]}')
-logging.info(f'Number of total samples: {mt.count()[1]}')
+vds = hl.read_vds(vds_path)
+logging.info(f'Number of total loci: {vds.count()[0]}')
+logging.info(f'Number of total samples: {vds.count()[1]}')
 
-mt = mt.filter_rows(
-    (hl.len(hl.or_else(mt.filters, hl.empty_set(hl.tstr))) == 0)  # QC
-    & (hl.len(mt.alleles) == 2)  # remove hom-ref
-    & (mt.n_unsplit_alleles == 2)  # biallelic (exclude multiallelic)
-    & (hl.is_snp(mt.alleles[0], mt.alleles[1]))  # SNPs (exclude indels)
+vds = vds.filter_rows(
+    (hl.len(hl.or_else(vds.filters, hl.empty_set(hl.tstr))) == 0)  # QC
+    & (hl.len(vds.alleles) == 2)  # remove hom-ref
+    & (vds.n_unsplit_alleles == 2)  # biallelic (exclude multiallelic)
+    & (hl.is_snp(vds.alleles[0], vds.alleles[1]))  # SNPs (exclude indels)
 )
 
-mt = hl.variant_qc(mt)
+vds = hl.variant_qc(vds)
 
 # common variants only
-cv_mt = mt.filter_rows(mt.variant_qc.AF[0] > 0.05)
+cv_vds = vds.filter_rows(vds.variant_qc.AF[0] > 0.05)
 
 # export to plink common variants only for sparse GRM
-export_vcf(cv_mt, cv_demux_vcf_path, ind_id=cv_mt.s)
+export_vcf(cv_vds, cv_demux_vcf_path, ind_id=cv_vds.s)
