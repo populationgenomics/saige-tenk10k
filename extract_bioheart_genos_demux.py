862df619d5dbfbbdeb87db472de0148886b7ed50
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2023-12-21 15:04:14 +1100

    disable for now as i dont understand

--- extract_bioheart_genos_demux.py
+++ extract_bioheart_genos_demux.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# pylint: disable=import-error,wrong-import-position
+# pylint: disable=import-error,missing-module-docstring,wrong-import-position
 
 __author__ = 'annacuomo'
 
