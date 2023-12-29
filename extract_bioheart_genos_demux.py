5a3ff29a07a4109dfe60614e621226b34447f483
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2023-12-29 12:26:48 +1100

    update script to reflect findings during testing

--- extract_bioheart_genos_demux.py
+++ extract_bioheart_genos_demux.py
@@ -35,15 +35,21 @@ HAIL_IMAGE = get_config()['images']['scanpy']
 init_batch()
 
 vds_path = BIOHEART_JOINT_CALL_VDS
-cv_demux_vcf_path = output_path('demux_vcf_common_variants')
+cv_demux_vcf_path = output_path('demux_vcf_common_variants.vcf.bgz')
 
 vds = hl.vds.read_vds(vds_path)
-mt = vds.variant_data
+
+# split multiallelic loci
+vds = hl.vds.split_multi(vds, filter_changed_loci=True)
+
+# mt = vds.variant_data
+mt = hl.vds.to_dense_mt(vds)
 logging.info(f'Number of total loci: {mt.count()[0]}')
 logging.info(f'Number of total samples: {mt.count()[1]}')
 
 mt = mt.filter_rows(
-    (mt.n_unsplit_alleles == 2)  # biallelic (exclude multiallelic)
+    mt.was_split  # biallelic (exclude multiallelic)
+    & (hl.len(mt.alleles) == 2)  # remove hom-ref
     & (hl.is_snp(mt.alleles[0], mt.alleles[1]))  # SNPs (exclude indels)
 )
 
@@ -52,5 +58,5 @@ mt = hl.variant_qc(mt)
 # common variants only
 cv_mt = mt.filter_rows(mt.variant_qc.AF[0] > 0.05)
 
-# export to plink common variants only for sparse GRM
-export_vcf(cv_mt, cv_demux_vcf_path, ind_id=cv_mt.s)
+# export to plink common variants only for demultiplexing
+export_vcf(cv_mt, cv_demux_vcf_path)
