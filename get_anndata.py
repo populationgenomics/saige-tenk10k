36737ef58bce7e8a89e45866710b163edaaa207d
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2024-01-31 12:06:39 +1100

    add comments as to what needs to be added

--- get_anndata.py
+++ get_anndata.py
@@ -20,15 +20,36 @@ from cpg_utils.hail_batch import (
     output_path,
 )
 import click
+import hail as hl
+import pandas as pd
 import scanpy as sc
 
 SCANPY_IMAGE = get_config()['images']['scanpy']
 
 
+def get_gene_cis_file(gene_info_df, gene: str, window_size: int):
+    """Get gene cis window file"""
+    # select the gene from df
+    gene_info_gene = gene_info_df[gene_info_df['gene'] == gene]
+    # get gene chromosome
+    chrom = gene_info_gene['chr']
+    # get gene body position (start and end) and add window
+    left_boundary = max(1, int(gene_info_gene['start']) - window_size)
+    right_boundary = min(
+        int(gene_info_gene['end']) + window_size,
+        hl.get_reference('GRCh38').lengths[chrom],
+    )
+    data = {'chromosome': chrom, 'start': left_boundary, 'end': right_boundary}
+    # check if I need an index at all
+    return pd.DataFrame(data, index=[gene])
+
+
 @click.command()
 @click.option('--celltypes')
 @click.option('--chromosomes')
+@click.option('--gene-info-tsv')
 @click.option('--expression-files-prefix')
+@click.option('--sample-mapping-file-path')
 @click.option('--min-pct-expr', type=int, default=5)
 @click.option('--cis-window-size', type=int, default=100000)
 @click.option(
@@ -44,7 +65,9 @@ SCANPY_IMAGE = get_config()['images']['scanpy']
 def main(
     celltypes: str,
     chromosomes: str,
+    gene_info_tsv: str,
     expression_files_prefix: str,
+    sample_mapping_file_path: str,
     min_pct_expr: int,
     cis_window_size: int,
     max_gene_concurrency=int,
@@ -52,10 +75,34 @@ def main(
     """
     Run expression processing pipeline
     """
-    expression_h5ad_path = to_path(
-        dataset_path(f'scrna-seq/CellRegMap_input_files/expression_objects/sce22.h5ad')
-    ).copy('here.h5ad')
-    expression_adata = sc.read(expression_h5ad_path)
+    # extract samples we actually want to test
+
+    # extract sample level covariates
+    # age from metamist
+    # sex from somalier
+
+    for celltype in celltypes.split(','):
+
+        # extract cell-level covariates
+        # expression PCs, cell type specific
+
+        for chromosome in chromosomes.split(','):
+            expression_h5ad_path = to_path(
+                dataset_path(f'{expression_files_prefix}/{celltype}_{chromosome}.h5ad')
+            ).copy('here.h5ad')
+            expression_adata = sc.read(expression_h5ad_path)
+
+            # extract genes expressed in at least X% cells
+
+            # for each gene
+
+            # get expression
+            # make pheno cov file
+
+            # get gene info
+            # make cis window file
+
+    get_batch().run(wait=False)
 
 
 if __name__ == '__main__':
