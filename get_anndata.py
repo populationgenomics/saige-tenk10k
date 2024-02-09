2f16496849bfe1c0f04c3b13a5bad5bee8016b29
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2024-02-09 13:17:29 +1100

    fix concat syntax

--- get_anndata.py
+++ get_anndata.py
@@ -112,7 +112,9 @@ def make_pheno_cov(
     print(expr_df.shape)
     print(sample_covs_df.shape)
     print(celltype_covs_df.shape)
-    pheno_cov_df = pd.concat(cell_ind_df, expr_df, sample_covs_df, celltype_covs_df)
+    pheno_cov_df = pd.concat(
+        [cell_ind_df, expr_df, sample_covs_df, celltype_covs_df], axis=1
+    )
     with to_path(out_path).open('w') as pcf:
         pheno_cov_df.to_csv(pcf, index=False)
 
