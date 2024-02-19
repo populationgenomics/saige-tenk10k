a789e9dccce48a6c4477123e2214b6ed9a377d0f
MattWellie <mattwellie@gmail.com>
2024-02-20 08:29:14 +1000

    quick change

--- get_genotype_vcf.py
+++ get_genotype_vcf.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# pylint: disable=import-error,name-defined
 
 """
 This script will
@@ -143,7 +142,7 @@ def main(
             bcftools_job.storage('15G')
             # now remove "chr" from chromosome names using bcftools
             bcftools_job.command(
-                'for num in {{1..22}} X Y; do echo "chr${num} ${num}" >> chr_update.txt; done'
+                'for num in {1..22} X Y; do echo "chr${num} ${num}" >> chr_update.txt; done'
             )
             bcftools_job.command(
                 f'bcftools annotate --rename-chrs chr_update.txt {vcf_input} -o {bcftools_job.vcf}'
