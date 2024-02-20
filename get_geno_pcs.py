171c4c6b4dd0d1197fd6d1cb39af49c85b978e1c
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-02-20 16:31:24 +1100

    replace mt with vds input

--- get_geno_pcs.py
+++ get_geno_pcs.py
@@ -11,7 +11,7 @@ analysis-runner \
     --description "extract geno PCs" \
     --dataset "bioheart" \
     --access-level "test" \
-    --output-dir "saige-qtl/input_files/covariates" \
+    --output-dir "saige-qtl/input_files/covariates/vds-input" \
     python3 get_geno_pcs.py --vds-version 1-0
 
 """
@@ -34,13 +34,8 @@ def main(vds_version):
     # load vds object
     vds_path = dataset_path(f'vds/{vds_version}.vds')
     vds = hl.vds.read_vds(vds_path)
-    # split multiallelic loci (this step is necessary for densifying below)
-    vds = hl.vds.split_multi(vds, filter_changed_loci=True)
-    # densify to matrix table object
-    mt = hl.vds.to_dense_mt(vds)
-    mt.write(output_path(f'{vds_version}/dense_matrix.mt'), overwrite=True)
-    # get pcs
-    _, scores, loadings = hl.hwe_normalized_pca(mt.GT, k=15, compute_loadings=True)
+        # get pcs
+    _, scores, loadings = hl.hwe_normalized_pca(vds.GT, k=15, compute_loadings=True)
     # write
     scores.write(output_path(f'{vds_version}/geno_pca_scores.ht'), overwrite=True)
     loadings.write(output_path(f'{vds_version}/geno_pca_loadings.ht'), overwrite=True)
