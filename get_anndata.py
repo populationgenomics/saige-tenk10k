798e940fb3538cf6d97dbf1b9dabfdb9d2ca31e3
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2024-02-29 10:57:04 +1100

    test on 100 genes

--- get_anndata.py
+++ get_anndata.py
@@ -263,7 +263,7 @@ def main(
                         writer.write(line)
 
             # start up some jobs for each gene
-            for gene in expression_adata.var['gene_name'][0:10]:
+            for gene in expression_adata.var['gene_name'][0:100]:
 
                 # make pheno cov file
                 pheno_cov_filename = to_path(
