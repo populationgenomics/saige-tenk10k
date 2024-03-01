0a68399c604f774cf7d92638584c0d1b5e64311f
MattWellie <mattwellie@gmail.com>
2024-03-01 10:38:31 +1000

    print a bunch of stuff

--- saige_assoc.py
+++ saige_assoc.py
@@ -82,6 +82,7 @@ def build_fit_null_command(
     Rscript command (str) ready to run (bash)
     """
     pheno_file = get_batch().read_input(pheno_file)
+    print(plink_path)
     plink_prefix = get_batch().read_input_group(
         bim=f'{plink_path}.bim', bed=f'{plink_path}.bed', fam=f'{plink_path}.fam'
     )
