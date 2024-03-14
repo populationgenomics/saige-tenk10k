ddbb7b7cd2a0e0d88d94401c334a07d313674154
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-03-14 17:15:07 +1100

    update docu

--- get_anndata.py
+++ get_anndata.py
@@ -17,7 +17,7 @@ analysis-runner \
     --description "make expression input files" \
     --dataset "bioheart" \
     --access-level "test" \
-    --output-dir "saige-qtl/input_files/hope-test33" \
+    --output-dir "saige-qtl/input_files" \
     --image australia-southeast1-docker.pkg.dev/cpg-common/images/scanpy:1.9.3 \
     python3 get_anndata.py --celltypes B_naive --chromosomes chr21
 
