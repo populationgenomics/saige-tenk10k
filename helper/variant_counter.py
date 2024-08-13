14108679f20e3e1047e453ced2feb96a5c168569
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2024-08-13 11:52:37 +1000

    mt to vds

--- helper/variant_counter.py
+++ helper/variant_counter.py
@@ -3,12 +3,10 @@
 # ruff: noqa: PLR2004
 
 """
-This script aims to count the total number of variants (non-ref)
-from the whole TOB-WGS dataset that are biallelic SNVs
-and rare (MAF < 5%)
+This script aims to count the total number of variants
+from a vds object provided (e.g. bioheart, tob-wgs or both)
 
-copy from tob-wgs/scripts/rv_expression_association/count_variants.py
-TO DO: update from MT to VDS
+similar to tob-wgs/scripts/rv_expression_association/count_variants.py
 """
 
 import click
@@ -19,30 +17,53 @@ from cpg_utils.hail_batch import dataset_path, init_batch
 
 
 @click.command()
-@click.option('--mt-path', required=True)  # 'mt/v7.mt'
-def count_variants(mt_path: str):
+@click.option('--vds-path', required=True)
+@click.option('--cv-maf-threshold', default=0.05)
+@click.option('--rv-maf-threshold', default=0.05)
+@click.option('--exclude-multiallelic', default=False)
+@click.option('--exclude-indels', default=False)
+def count_variants(
+    vds_path: str,
+    cv_maf_threshold: float,
+    rv_maf_threshold: float,
+    exclude_multiallelic: bool,
+    exclude_indels: bool,
+):
     """
-    reads the MT, conducts QC filtering, and prints the nuber
-    of remaining variants
+    reads the VDS, converts to MT,
+    if set to true exclude indels and multiallelic snps
+    and prints the number of remaining variants
+    split between common and rare at given thresholds
     """
-    # read hail matrix table object (WGS data)
+    # read VDS object (WGS data)
     init_batch()
-    mt = hl.read_matrix_table(dataset_path(mt_path))
-    mt = hl.experimental.densify(mt)
+    vds = hl.vds.read_vds(dataset_path(vds_path))
+    vds = hl.vds.split_multi(vds, filter_changed_loci=True)
+    # convert to hail matrix table
+    mt = hl.vds.to_dense_mt(vds)
+    vds_name = vds_path.split('/')[-1].split('.')[0]
+    print(f'Counting variants for {vds_name}')
 
-    # filter out low quality variants and consider biallelic variants only (no multi-allelic, no ref-only)
-    mt = mt.filter_rows(
-        (hl.len(hl.or_else(mt.filters, hl.empty_set(hl.tstr))) == 0)  # QC
-        & (hl.len(mt.alleles) == 2)  # remove hom-ref
-        & (mt.n_unsplit_alleles == 2)  # biallelic
-        & (hl.is_snp(mt.alleles[0], mt.alleles[1])),  # SNVs
-    )
+    # filter out loci & variant QC
+    mt = mt.filter_rows(hl.len(mt.alleles) == 2)  # remove hom-ref
+    if exclude_multiallelic:  # biallelic only (exclude multiallelic)
+        print(f'Number of variants before excluding multiallelic loci: {mt.count()[0]}')
+        mt = mt.filter_rows(~(mt.was_split))
+        print(f'Number of variants after excluding multiallelic loci: {mt.count()[0]}')
+    if exclude_indels:  # SNPs only (exclude indels)
+        print(f'Number of variants before excluding indels: {mt.count()[0]}')
+        mt = mt.filter_rows(hl.is_snp(mt.alleles[0], mt.alleles[1]))
+        print(f'Number of variants after excluding indels: {mt.count()[0]}')
 
-    # filter rare variants only (MAF < 5%)
+    # compute allele frequencies as part of variant qc
     mt = hl.variant_qc(mt)
-    mt = mt.filter_rows((mt.variant_qc.AF[1] < 0.05) & (mt.variant_qc.AF[1] > 0))
 
-    print(mt.count())
+    # select common and rare variants
+    cv_mt = mt.filter_rows(mt.variant_qc.AF[1] >= cv_maf_threshold)
+    rv_mt = mt.filter_rows(mt.variant_qc.AF[1] < rv_maf_threshold)
+
+    print(f'Common variant (MAF>={cv_maf_threshold}) count: {cv_mt.count()[0]}')
+    print(f'Rare variant (MAF<{rv_maf_threshold}) count: {rv_mt.count()[0]}')
 
 
 if __name__ == '__main__':
