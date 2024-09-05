66fb50d6f4bfe4bc8689efaf343ada4fd715cddc
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2024-09-06 09:58:05 +1000

    add summarising

---
+++ plotter/summarise_and_qq_plotter.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+"""
+This script summarise SAIGE-QTL raw p-value results
+and plots QQ plots and histograms of the p-values
+
+To run:
+
+analysis-runner \
+    --description "plot p-values" \
+    --dataset "bioheart" \
+    --access-level "test" \
+    --output-dir "saige-qtl/" \
+    python3 summarise_and_qq_plotter.py \
+        --celltype='B_naive' \
+        --results-path=gs://cpg-bioheart-main-analysis/saige-qtl/bioheart_n990_and_tob_n1055/output_files/sample_perm0/output_files \
+        --title='Shuffled p-values (SAIGE-QTL pipeline)'
+"""
+
+import click
+import pandas as pd
+from cpg_utils import to_path
+
+# import hail as hl
+
+# from cpg_utils.hail_batch import init_batch, output_path
+# from bokeh.plotting import output_file, save
+
+
+@click.command()
+@click.option('--celltype', required=True)
+@click.option('--results-path', required=True)
+# @click.option('--title', default='SAIGE-QTL pipeline p-values')
+def plot_pvalues(
+    celltype: str,
+    results_path: str,
+    # title: str,
+):
+    """
+    combines the results for a given cell type,
+    saving both all results and top SNP per gene results
+    plots both a histogram and a QQ plot of the association p-values
+    """
+    # init_batch()
+
+    # collect all raw p-value files
+    existing_cv_assoc_results = [
+        str(file) for file in to_path(results_path).glob(f'{celltype}_*_cis')
+    ]
+    # concatenates the dataframes using pandas
+    results_all_df = []
+    results_top_snp_df = []
+    for pv_df in existing_cv_assoc_results:
+        df = pd.read_csv(to_path(pv_df), sep='\t')
+        # add gene as column before merging
+        df['gene'] = (
+            pv_df.replace(f'{results_path}/', '')
+            .replace(f'{celltype}_', '')
+            .replace('_cis', '')
+        )
+        # add SNP info
+        df['is_snp'] = [
+            len(df['Allele1'].values[i]) == 1 and len(df['Allele2'].values[i]) == 1
+            for i in range(df.shape[0])
+        ]
+        results_all_df.append(df)
+        # select top SNP
+        df_snp = df[df['is_snp']]
+        df_top_snp = df_snp[df_snp['p.value'] == df_snp['p.value'].min()]
+        results_top_snp_df.append(df_top_snp)
+    results_all_df = pd.concat(results_all_df)
+    results_top_snp_df = pd.concat(results_top_snp_df)
+
+    # save
+    results_all_file = (
+        f'{results_path}/summary_stats/{celltype}_all_cis_raw_pvalues.tsv'
+    )
+    results_all_df.to_csv(results_all_file, sep='\t')
+
+    results_top_snp_file = (
+        f'{results_path}/summary_stats/{celltype}_top_snp_cis_raw_pvalues.tsv'
+    )
+    results_top_snp_df.to_csv(results_top_snp_file, sep='\t')
+
+    # p = hl.plot.histogram(mt.variant_qc.AF[1], legend=title)
+    # output_file('local_histo.html')
+    # save(p)
+    # gcs_path_p = output_path(f'plots/frequency_histo/{vds_name}.html', 'analysis')
+    # hl.hadoop_copy('local_histo.html', gcs_path_p)
+
+
+if __name__ == '__main__':
+    plot_pvalues()
