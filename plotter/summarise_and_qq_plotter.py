eade79dc8e7b8d168226fb783d9a0b57f0e3f0cb
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2024-09-06 13:32:51 +1000

    test saving syntax

--- plotter/summarise_and_qq_plotter.py
+++ plotter/summarise_and_qq_plotter.py
@@ -84,39 +84,49 @@ def plot_pvalues(
     )
     results_top_snp_df.to_csv(results_top_snp_file, sep='\t')
 
-    # plot histograms
-    p_hist_all = plt.hist(results_all_df['p.value'])
-    p_hist_top = plt.hist(results_top_snp_df['p.value'])
+    # # plot histograms
+    # p_hist_all = plt.hist(results_all_df['p.value'])
+    # p_hist_top = plt.hist(results_top_snp_df['p.value'])
 
-    # save histograms
-    fig, _ = plt.subplots(figsize=(10, 8))
-    fig.save(p_hist_all)
-    gcs_path_p = output_path(
-        f'plots/pvalues_histo/{celltype}_shuffled.html', 'analysis'
-    )
-    hl.hadoop_copy('local_histo.html', gcs_path_p)
+    # # save histograms
+    # fig, _ = plt.subplots(figsize=(10, 8))
+    # fig.save(p_hist_all)
+    # gcs_path_p = output_path(
+    #     f'plots/pvalues_histo/{celltype}_shuffled.html', 'analysis'
+    # )
+    # hl.hadoop_copy('local_histo.html', gcs_path_p)
 
     # QQ plots
     expected_pvals_all = np.random.uniform(low=0, high=1, size=results_all_df.shape[0])
-    expected_pvals_top = np.random.uniform(
-        low=0, high=1, size=results_top_snp_df.shape[0]
-    )
+    # expected_pvals_top = np.random.uniform(
+    #     low=0, high=1, size=results_top_snp_df.shape[0]
+    # )
     # all results
     x_all = -np.log10(np.sort(expected_pvals_all))
     y_all = -np.log10(np.sort(results_all_df['p.value']))
-    p_qq_all = plt.scatter(x_all, y_all)
-    # top SNP
-    x_top = -np.log10(np.sort(expected_pvals_top))
-    y_top = -np.log10(np.sort(results_top_snp_df['p.value']))
-    p_qq_top_snp = plt.scatter(x_top, y_top)
 
-    # save qq plots
-    fig, _ = plt.subplots(figsize=(10, 8))
-    fig.save(p_hist_all)
+    plt.figure(figsize=(8, 8))
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.scatter(x_all, y_all)
+    fig.tight_layout()
+    fig.savefig('qqplot.png')
     gcs_path_p = output_path(
-        f'plots/pvalues_histo/{celltype}_shuffled.html', 'analysis'
+        f'plots/pvalues_qqplot/{celltype}_shuffled_all.html', 'analysis'
     )
-    hl.hadoop_copy('local_histo.html', gcs_path_p)
+    hl.hadoop_copy('qqplot.png', gcs_path_p)
+
+    # # top SNP
+    # x_top = -np.log10(np.sort(expected_pvals_top))
+    # y_top = -np.log10(np.sort(results_top_snp_df['p.value']))
+    # p_qq_top_snp = plt.scatter(x_top, y_top)
+
+    # # save qq plots
+
+    # fig.save(p_hist_all)
+    # gcs_path_p = output_path(
+    #     f'plots/pvalues_histo/{celltype}_shuffled.html', 'analysis'
+    # )
+    # hl.hadoop_copy('local_histo.html', gcs_path_p)
 
 
 if __name__ == '__main__':
