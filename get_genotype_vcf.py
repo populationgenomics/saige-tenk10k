5931504572a76c12272f924ecc3bcae1f3ba8b2c
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2024-09-23 12:22:52 +1000

    only subset if necessary

--- get_genotype_vcf.py
+++ get_genotype_vcf.py
@@ -370,8 +370,9 @@ def main(
 
         # since pruning is very costly, subset first a bit
         random.seed(0)
-        vre_mt = vre_mt.sample_rows(p=0.01)
-        logging.info('subset completed')
+        if n_ac_vars > {vre_n_markers * 100}:
+            vre_mt = vre_mt.sample_rows(p=0.01)
+            logging.info('subset completed')
 
         # perform LD pruning
         pruned_variant_table = hl.ld_prune(
