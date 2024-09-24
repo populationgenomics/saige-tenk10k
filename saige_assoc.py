92a771cd56cc1ea26e9d4dc5d33fb26c85d1b2ae
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2024-09-24 16:14:49 +1000

    black

--- saige_assoc.py
+++ saige_assoc.py
@@ -374,7 +374,9 @@ def main(
             )
 
         # read in vcf file once per chromosome
-        vcf_group = get_batch().read_input_group(vcf=vcf_file_path, index=f'{vcf_file_path}.csi')
+        vcf_group = get_batch().read_input_group(
+            vcf=vcf_file_path, index=f'{vcf_file_path}.csi'
+        )
 
         # cis window files are split by gene but organised by chromosome also
         cis_window_files_path_chrom = f'{cis_window_files_path}/{chromosome}'
@@ -429,7 +431,9 @@ def main(
                     gene_dependency = null_job
 
                 # step 2 (cis eQTL single variant test)
-                sv_out_path = output_path(f'{celltype}/{chromosome}/{celltype}_{gene}_cis', 'analysis')
+                sv_out_path = output_path(
+                    f'{celltype}/{chromosome}/{celltype}_{gene}_cis', 'analysis'
+                )
                 if to_path(sv_out_path).exists():
                     step2_output = get_batch().read_input(sv_out_path)
                 else:
