88d529a9db8bbd3f813a7337a6a6583aabe0cf0d
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2024-10-16 16:09:42 +1100

    main instructions

--- make_group_file.py
+++ make_group_file.py
@@ -12,6 +12,18 @@ SAIGE-QTL association pipeline.
 
 To run:
 
+In main:
+
+analysis-runner \
+    --description "make variant group input files" \
+    --dataset "bioheart" \
+    --access-level "standard" \
+    --output-dir "saige-qtl/bioheart_n787_and_tob_n960/241008_ashg/input_files/" \
+    python3 make_group_file.py --chromosomes chr21 \
+        --cis-window-files-path=gs://cpg-bioheart-main/saige-qtl/bioheart_n787_and_tob_n960/241008_ashg/input_files/cis_window_files/ \
+        --group-files-path=gs://cpg-bioheart-main/saige-qtl/bioheart_n787_and_tob_n960/241008_ashg/input_files/group_files_mt/ \
+        --chrom-mt-files-path=gs://cpg-bioheart-main/saige-qtl/bioheart_n787_and_tob_n960/241008_ashg/input_files/genotypes/vds-tenk10k1-0_qc_pass --max-delay=30
+
 In test:
 
 analysis-runner \
