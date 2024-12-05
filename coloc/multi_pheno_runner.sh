ab530121c524eeab9fec86515f8a16d1fd38ba3b
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2024-12-05 15:40:13 +1100

    clean up

--- coloc/multi_pheno_runner.sh
+++ coloc/multi_pheno_runner.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-
 # Define the cell types as a comma-separated string
 celltypes='B_intermediate,ILC,Plasmablast,ASDC,cDC1,pDC,NK_CD56bright,MAIT,B_memory,CD4_CTL,CD4_Proliferating,CD8_Proliferating,HSPC,NK_Proliferating,cDC2,CD16_Mono,Treg,CD14_Mono,CD8_TCM,CD4_TEM,CD8_Naive,NK,CD8_TEM,CD4_Naive,B_naive,gdT,dnT,CD4_TCM'
 
 # Convert the cell types into an array
 IFS=',' read -ra celltype_array <<< "$celltypes"
-
-# create phenotype array
-# ???
 
 # Loop through each phenotype
 for i in \
@@ -33,7 +29,7 @@ for i in \
     ; do
     # Loop through each cell type
     for celltype in "${celltype_array[@]}"; do
-        echo "Running coloc analysis for cell type: $celltype, phenotype: $pheno_name"
+        echo "Running coloc analysis for cell type: $celltype, phenotype: $1"
         analysis-runner --dataset "bioheart" \
         --description "Run coloc for eGenes identified by SAIGE-QTL analysis" \
         --access-level "full" \
@@ -41,8 +37,8 @@ for i in \
         --image "australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:d4922e3062565ff160ac2ed62dcdf2fba576b75a-hail-8f6797b033d2e102575c40166cf0c977e91f834e" \
         --output-dir "saige-qtl/bioheart_n787_and_tob_n960/241008_ashg/coloc-snp-only/sig_genes_only/" \
         coloc/coloc_runner.py \
-        --snp-gwas-file="$pheno_gwas_file" \
-        --pheno-output-name="$pheno_name" \
+        --snp-gwas-file="$2" \
+        --pheno-output-name="$1" \
         --celltypes="$celltype"
     done
 done
