500d2c83c1f64012d48a19e5d0e068683a7eacb0
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2024-12-31 11:26:37 +1100

    adapt multi runner

--- coloc/multi_pheno_ukbb_coloc_runner.sh
+++ coloc/multi_pheno_ukbb_coloc_runner.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Define the cell types as a comma-separated string
-#celltypes='ILC,Plasmablast,ASDC,cDC1,pDC,NK_CD56bright,MAIT,B_memory,CD4_CTL,CD4_Proliferating,CD8_Proliferating,HSPC,NK_Proliferating,cDC2,CD16_Mono,Treg,CD14_Mono,CD8_TCM,CD4_TEM,CD8_Naive,NK,CD8_TEM,CD4_Naive,B_naive,gdT,dnT,CD4_TCM'
-celltypes='B_intermediate'
+celltypes='ILC,Plasmablast,ASDC,cDC1,pDC,NK_CD56bright,MAIT,B_memory,CD4_CTL,CD4_Proliferating,CD8_Proliferating,HSPC,NK_Proliferating,cDC2,CD16_Mono,Treg,CD14_Mono,CD8_TCM,CD4_TEM,CD8_Naive,NK,CD8_TEM,CD4_Naive,B_naive,gdT,dnT,CD4_TCM'
 
 # Convert the cell types into an array
 IFS=',' read -ra celltype_array <<< "$celltypes"
@@ -58,14 +57,17 @@ phenotypes=(
 # Loop through each cell type and phenotype
 for celltype in "${celltype_array[@]}"; do
     for pheno in "${phenotypes[@]}"; do
-        analysis-runner --dataset "bioheart" \
-        --description "Run coloc for eGenes identified by STR analysis" \
-        --access-level "test" \
-        --memory='4G' \
+        analysis-runner --dataset "tenk10k" \
+        --description "Run coloc for eGenes identified by SAIGE-QTL" \
+        --access-level "full" \
+        --memory='8G' \
         --image "australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:d4922e3062565ff160ac2ed62dcdf2fba576b75a-hail-8f6797b033d2e102575c40166cf0c977e91f834e" \
-        --output-dir "tenk10k/str/associatr/final_freeze" \
-        coloc_ukbb_runner.py \
+        --output-dir "saige-qtl/tenk10k-genome-2-3-eur/input_files/241210" \
+        coloc/coloc_ukbb_runner.py \
         --pheno-output-name="$pheno" \
+        --egenes-files-path=gs://cpg-tenk10k-main-analysis/saige-qtl/tenk10k-genome-2-3-eur/output_files/241210/summary_stats \
+        --snp-cis-dir=gs://cpg-tenk10k-main-analysis/saige-qtl/tenk10k-genome-2-3-eur/output_files/241210 \
+        --snp-gwas-file="$filepath" \
         --celltypes "$celltype" \
         --max-parallel-jobs 10000
     done
